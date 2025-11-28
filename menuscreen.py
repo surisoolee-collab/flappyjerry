dda7e3ac66524d498fa7dd1b6ba765b1cc945ec1
gulyarudun-art <gulyarudun@gmail.com>
2025-11-27 23:03:13 -0500

    Update menuscreen.py

--- menuscreen.py
+++ menuscreen.py
@@ -34,6 +34,8 @@ def run_menu():
             # Title text
             draw_text('Flappy Jerry', font, text_color, SCREEN, 100, 100)
 
+            highscore = 0 
+            
             draw_text(f"High Score: {highscore}", font, text_color, SCREEN, 100, 200)
 
             # START button clicked → return to main game
